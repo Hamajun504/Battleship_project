b4683ea333f160f17b39313ffbe1536dbf3c99c3
Yaroslav Orlov <orlov.iaa@phystech.edu>
2022-11-24 18:43:48 +0300

    init

---
+++ game.py
@@ -0,0 +1,12 @@
+class Game:
+    def __init__(self):
+        self.step = 1
+
+
+    def next_step(self):
+        self.step += 1
+
+    def draw(self):
+        pass
+
+    def
---
+++ ships.py
@@ -0,0 +1,19 @@
+class Ship:
+    def draw(self):
+        pass
+
+
+class Single(Ship):
+    pass
+
+
+class Double(Ship):
+    pass
+
+
+class Triple(Ship):
+    pass
+
+
+class Quadriple(Ship):
+    pass
