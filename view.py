b768a9660fb8f020ae433424bf7eb379e9c88779
Yaroslav Orlov <orlov.iaa@phystech.edu>
2022-12-04 16:23:54 +0300

    параметры полей хранятся в view

--- view.py
+++ view.py
@@ -9,20 +9,23 @@ class View:
         self.turn = turn
         self.screen = screen
 
-        self.grid = pygame.Surface((config.FIELD_RECT1[2] + 1, config.FIELD_RECT1[3] + 1))
+        self.field_size = config.FIELD_SiZE
+        self.fields_pos = config.FIELDS_POS
+
+        self.grid = pygame.Surface((self.field_size[0] + 1, self.field_size[1] + 1))
         self.grid.fill(config.WHITE)
         for i in range(11):
             pygame.draw.line(self.grid, config.GREY,
-                             (0, config.FIELD_RECT1[3] * i // 10),
-                             (config.FIELD_RECT1[2], config.FIELD_RECT1[3] * i // 10), 2)
+                             (0, self.field_size[1] * i // 10),
+                             (self.field_size[0], self.field_size[1] * i // 10), 2)
             pygame.draw.line(self.grid, config.GREY,
-                             (config.FIELD_RECT1[2] * i // 10, 0),
-                             (config.FIELD_RECT1[2] * i // 10, config.FIELD_RECT1[3]), 2)
+                             (self.field_size[0] * i // 10, 0),
+                             (self.field_size[0] * i // 10, self.field_size[1]), 2)
 
     def draw(self):
         self.screen.fill(config.WHITE)
-        self.screen.blit(self.grid, (config.FIELD_RECT1[0], config.FIELD_RECT1[1]))
-        self.screen.blit(self.grid, (config.FIELD_RECT2[0], config.FIELD_RECT2[1]))
+        self.screen.blit(self.grid, self.fields_pos[0])
+        self.screen.blit(self.grid, self.fields_pos[1])
         self.draw_ships()
         pygame.display.update()
 
